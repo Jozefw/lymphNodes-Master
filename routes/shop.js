51712d872c5d18fd93cabd19714ff961d4ee121e
Jozef <Jozefw@sbcglobal.net>
2023-08-24 14:18:59 -0700

    add controller fro cart and checkout

--- routes/shop.js
+++ routes/shop.js
@@ -3,9 +3,9 @@ const express = require('express');
 const shopController = require('../controllers/products')
 const router = express.Router();
 
-router.get('/', shopController.getProducts);
-router.get('/products');
-router.get('/cart');
-router.get('/checkout');
+router.get('/', shopController.getIndex);
+router.get('/products',shopController.getProducts);
+router.get('/cart',shopController.getCart);
+router.get('/checkout',shopController.getCheckout);
 
 module.exports = router;
